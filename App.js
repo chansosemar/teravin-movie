426f3dfb0fd1e7852eeffe9ce46b5137164aa159
Chandra Santoso <chansosemar@gmail.com>
2021-01-13 01:24:35 +0700

    prepare Splashscreen

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {StyleSheet, View, Text, SafeAreaView} from 'react-native';
+import {SplashScreen} from './src/components'
 import Router from './src/navigator';
 
 const App: () => React$Node = () => {
+	const [loading, setLoading] = useState(true)
+
+
 	return (
 		<>
-			<Router />
+		<Router/>
+		
 		</>
 	);
 };
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,9 @@
 import OfflineNotif from './offlineNotif'
 import ListMovie from './listMovie'
+import SplashScreen from './splashScreen'
 
 export {
 	OfflineNotif,
 	ListMovie,
+	SplashScreen
 }
